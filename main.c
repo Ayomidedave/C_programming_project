e157020ee71a36624a13ca243b8e8bb2f05bfcdd
danimarcos10 <133877225+danimarcos10@users.noreply.github.com>
2023-12-13 19:21:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,13 @@
 #include <stdio.h>
 #include "lexer.h"
+#include "parser.h"
 
 int main()
 {
     const char *code = "encrypt archive sync create delete search";
 
     initialize_lexer(code);
-
-    Token token;
-    do
-    {
-        token = get_next_token();
-        switch (token.type)
-        {
-        case ENCRYPT:
-        case ARCHIVE:
-        case SYNC:
-        case CREATE:
-        case DELETE:
-        case SEARCH:
-            printf("Command: %s\nDescription: %s\n", keywords[token.type], commands[token.type]);
-            break;
-        case END_OF_FILE:
-            printf("End of File\n");
-            break;
-        case INVALID_TOKEN:
-            printf("Invalid Token: %s\n", token.lexeme);
-            break;
-        default:
-            printf("Unknown Token\n");
-        }
-    } while (token.type != END_OF_FILE);
+    parse_program();
 
     return 0;
 }
